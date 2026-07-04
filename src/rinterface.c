c903cf0e20e4db8b1a89d4e545c6372135145705
Daniela <danielsalinascastro@gmail.com>
2026-07-04 00:01:48 -0400

    Agrega campo multilevel_modules de prueba a cluster_infomap

--- src/rinterface.c
+++ src/rinterface.c
@@ -12661,6 +12661,7 @@ SEXP R_igraph_community_infomap(SEXP graph, SEXP e_weights, SEXP v_weights, SEXP
   igraph_real_t c_codelength;
   SEXP membership;
   SEXP codelength;
+  SEXP multilevel_modules;
 
   SEXP r_result, r_names;
                                         /* Convert input */
@@ -12679,19 +12680,23 @@ SEXP R_igraph_community_infomap(SEXP graph, SEXP e_weights, SEXP v_weights, SEXP
   IGRAPH_R_CHECK(igraph_community_infomap(&c_graph, (Rf_isNull(e_weights) ? 0 : &c_e_weights), (Rf_isNull(v_weights) ? 0 : &c_v_weights), c_nb_trials, &c_membership, &c_codelength));
 
                                         /* Convert output */
-  PROTECT(r_result=NEW_LIST(2));
-  PROTECT(r_names=NEW_CHARACTER(2));
+  PROTECT(r_result=NEW_LIST(3));
+  PROTECT(r_names=NEW_CHARACTER(3));
   PROTECT(membership=Ry_igraph_vector_int_to_SEXP(&c_membership));
   igraph_vector_int_destroy(&c_membership);
   IGRAPH_FINALLY_CLEAN(1);
   PROTECT(codelength=NEW_NUMERIC(1));
   REAL(codelength)[0]=c_codelength;
+  PROTECT(multilevel_modules=NEW_INTEGER(1));
+  INTEGER(multilevel_modules)[0]=999;
   SET_VECTOR_ELT(r_result, 0, membership);
   SET_VECTOR_ELT(r_result, 1, codelength);
+  SET_VECTOR_ELT(r_result, 2, multilevel_modules);
   SET_STRING_ELT(r_names, 0, Rf_mkChar("membership"));
   SET_STRING_ELT(r_names, 1, Rf_mkChar("codelength"));
+  SET_STRING_ELT(r_names, 2, Rf_mkChar("multilevel_modules"));
   SET_NAMES(r_result, r_names);
-  UNPROTECT(3);
+  UNPROTECT(4);
 
   UNPROTECT(1);
   return(r_result);
